43e0719087e6e21efa03fd35418d1df9e6f7eae3
Maalik McBurrows <mmcburrows1@gmail.com>
2020-02-11 00:14:07 -0500

    added day 3

---
+++ DC Week 1 - Day3/magic_number.py
@@ -0,0 +1,13 @@
+# Prompt the user for a numer and compare that value to a pre defined value
+# compare that number to a predefined value
+# If the numbers match, tell the user theyre a mind reader
+# If they dont match, tell the user "thanks for playing"
+
+iser_input = int(input("What muber am I thinking of"))
+
+MAGICNUMBER = 9 
+
+if user_number ==  MAGICNUMBER:
+    print("Are you a mindreader?")
+else: 
+    print ("Thanks for playing, thats not correct")    
